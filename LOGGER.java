00d9238b2a94b8b9a403a225a9f81800313e5fa2
AM71113363 <71590903+AM71113363@users.noreply.github.com>
2024-09-07 21:27:09 +0200

    Update LOGGER.java

--- LOGGER.java
+++ LOGGER.java
@@ -1,4 +1,4 @@
-package sss.am71113363.logger;
+package put.your.package.here;
 
 import android.widget.*;
 import java.net.*;
@@ -7,80 +7,78 @@ import java.nio.charset.*;
 
 public abstract class LOGGER
 {
-	final static int PORT= 19132;
+    final static int PORT= 19132;
     static InetAddress broadcast=null;
-	static DatagramSocket dgram=null;
-	static Context ctx=null;
+    static DatagramSocket dgram=null;
+    static Context ctx=null;
 	
-	public static void start(Context c)
+     public static void start(Context c)
+     {
+	ctx=c;
+	new Thread(new Runnable()
 	{
-		ctx=c;
-		new Thread(new Runnable()
-		{
-			public void run()
-			{
-	           CreateSocket();
-	        }
-		}).start();
+	    public void run()
+	    {
+	        CreateSocket();
+	    }
+	}).start();
+     }
+     static void CreateSocket()
+     {
+	try
+	{
+	    dgram = new DatagramSocket(PORT);
+	    broadcast = InetAddress.getByName("255.255.255.255");
+	    dgram.setReuseAddress(true);
+	    dgram.setSoTimeout(4000);
+	    dgram.connect( broadcast,PORT);
+	}catch(Throwable e)
+	{ 
+	    Toast.makeText(ctx,e.toString(),1).show();
+	    if(dgram==null)
+		return;
+	    try
+	    {
+		if(!dgram.isClosed())
+			dgram.close();
+	    }catch(Throwable ee){}
+	    dgram=null
 	}
-
-	static void CreateSocket()
+     }
+     static void SendMessage(final String s)
+     {
+	if( dgram==null || broadcast==null)
+		return;
+	new Thread(new Runnable()
 	{
+	    public void run()
+	    {
 		try
 		{
-			dgram = new DatagramSocket(PORT);
-			broadcast = InetAddress.getByName("255.255.255.255");
-			dgram.setReuseAddress(true);
-			dgram.setSoTimeout(4000);
-			dgram.connect( broadcast,PORT);
-		}catch(Throwable e)
-		{ 
-			Toast.makeText(ctx,e.toString(),1).show();
-			if(dgram==null)
-				return;
-			try
-			{
-		    if(!dgram.isClosed())
-				dgram.close();
-			dgram=null;
-			}catch(Throwable ee){}
-		}
-	}
-	
-	static void SendMessage(final String s)
-	{
-		if( dgram==null || broadcast==null)
-			return;
-		new Thread(new Runnable()
-		{
-			public void run()
-			{
-				try
-				{
 		byte[] b=s.getBytes(StandardCharsets.UTF_8);
 		DatagramPacket log=new DatagramPacket(b,b.length,broadcast,PORT);
         dgram.send(log);
-                 }catch(Throwable e){}
-			 }
-		}).start();
+                }catch(Throwable e){}
+	    }
+	 }).start();
 	}
 
 	static void close()
 	{
-		if(dgram== null)
-			return;
-		new Thread(new Runnable()
-			{
-				public void run()
-				{
-					try
-					{
-						if(!dgram.isClosed())
-							dgram.close();
-					}catch(Throwable e){}
-					  dgram=null;
-				}
-			}).start();
+	    if(dgram== null)
+		return;
+	    new Thread(new Runnable()
+	    {
+		public void run()
+		{
+		    try
+		    {
+			if(!dgram.isClosed())
+			   dgram.close();
+		   }catch(Throwable e){}
+		   dgram=null;
+		}
+	    }).start();
 	}
 }
 
