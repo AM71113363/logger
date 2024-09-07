209fcb15902487bcd7388bebce37fa89e665d4a6
AM71113363 <71590903+AM71113363@users.noreply.github.com>
2024-09-07 10:59:09 +0200

    Add files via upload

---
+++ LOGGER.java
@@ -0,0 +1,86 @@
+package sss.am71113363.logger;
+
+import android.widget.*;
+import java.net.*;
+import android.content.*;
+import java.nio.charset.*;
+
+public abstract class LOGGER
+{
+	final static int PORT= 19132;
+    static InetAddress broadcast=null;
+	static DatagramSocket dgram=null;
+	static Context ctx=null;
+	
+	public static void start(Context c)
+	{
+		ctx=c;
+		new Thread(new Runnable()
+		{
+			public void run()
+			{
+	           CreateSocket();
+	        }
+		}).start();
+	}
+
+	static void CreateSocket()
+	{
+		try
+		{
+			dgram = new DatagramSocket(PORT);
+			broadcast = InetAddress.getByName("255.255.255.255");
+			dgram.setReuseAddress(true);
+			dgram.setSoTimeout(4000);
+			dgram.connect( broadcast,PORT);
+		}catch(Throwable e)
+		{ 
+			Toast.makeText(ctx,e.toString(),1).show();
+			if(dgram==null)
+				return;
+			try
+			{
+		    if(!dgram.isClosed())
+				dgram.close();
+			dgram=null;
+			}catch(Throwable ee){}
+		}
+	}
+	
+	static void SendMessage(final String s)
+	{
+		if( dgram==null || broadcast==null)
+			return;
+		new Thread(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+		byte[] b=s.getBytes(StandardCharsets.UTF_8);
+		DatagramPacket log=new DatagramPacket(b,b.length,broadcast,PORT);
+        dgram.send(log);
+                 }catch(Throwable e){}
+			 }
+		}).start();
+	}
+
+	static void close()
+	{
+		if(dgram== null)
+			return;
+		new Thread(new Runnable()
+			{
+				public void run()
+				{
+					try
+					{
+						if(!dgram.isClosed())
+							dgram.close();
+					}catch(Throwable e){}
+					  dgram=null;
+				}
+			}).start();
+	}
+}
+
---
+++ MainActivity.java
@@ -0,0 +1,206 @@
+package sss.am71113363.logger;
+
+import android.app.Activity;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import java.net.*;
+import java.io.*;
+import android.os.*;
+import android.graphics.*;
+
+public class MainActivity extends Activity
+{
+    private Button startServer;
+	static LinearLayout container;
+	
+	private static float dpi=2.625f;
+	
+	private int dpi2px(float px)
+	{
+		Float calc=Float.valueOf(Math.round(dpi*px));
+		return calc.intValue();
+	}
+  
+    private Handler hnd=new Handler()
+	{
+		public void handleMessage(Message msg)
+		{
+			 Bundle b=msg.getData();
+			 if(b!=null)
+			  {
+				   Object o= b.get("LOG");
+				   if(o!=null)
+				        addElement(o.toString());
+			   }
+	   }
+    };
+	
+	private byte[] rcv=new byte[512];
+	DatagramSocket dgram=null;
+	private DatagramSocket begin()
+	{
+		try
+		{
+			dgram = new DatagramSocket(19132);
+			dgram.setReuseAddress(true);
+		}catch(Throwable e)
+		{ 
+		   startServer.setText("Start Server");
+		   addElement(e.toString());
+		   return null;
+		}
+		return dgram;
+	}
+  private void listen()
+  {
+	  final DatagramSocket udp=begin();
+	  if(udp==null)
+		  return;
+	  startServer.setText("Stop");
+			  
+	  while(true)
+	  {
+		 try
+		 {
+			DatagramPacket pack=new DatagramPacket(rcv,rcv.length);
+            udp.receive(pack);
+			if(pack.getLength()>0)
+			{
+				String str=new String(rcv,0,pack.getLength());
+				Message ms=new Message();
+				Bundle data=new Bundle();
+				data.putString("LOG",str);
+				ms.setData(data);
+				hnd.sendMessage(ms);
+			}
+		 }catch(IOException e){break;}
+	 }
+	 startServer.setText("Start Server");
+     if(udp!=null)
+	 {
+	 try{ udp.close();}catch(Throwable e){}
+	 dgram=null;
+	 }
+	}
+	
+	public void start()
+	{
+		new Thread(new Runnable(){public void run(){listen();}}).start();
+	}
+	
+	private void CloseSocket()
+	{
+		if(dgram==null)
+			return;
+		try
+		{
+			if(dgram!=null)
+				dgram.close();
+		}catch(Throwable e){}
+		dgram=null;
+	}
+	public void close()
+	{
+		new Thread(new Runnable(){public void run(){CloseSocket();}}).start();
+	}
+	
+	
+	public void addElement(String msg)
+	{
+		FrameLayout f=new FrameLayout(this);
+		FrameLayout.LayoutParams txtPrm=new FrameLayout.LayoutParams(-1,-2);
+		txtPrm.bottomMargin=dpi2px(20);
+		txtPrm.leftMargin=dpi2px(2);
+		txtPrm.rightMargin=dpi2px(2);
+		LinearLayout.LayoutParams fp=new LinearLayout.LayoutParams(-1,-2);
+		TextView txt=new TextView(this);
+		txt.setTextSize((float)dpi2px(10));
+		txt.setText(msg);
+		f.addView(txt,txtPrm);
+		container.addView(f,fp);
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		dpi=this.getResources().getDisplayMetrics().density;
+
+
+		FrameLayout frm=new FrameLayout(this);
+		FrameLayout.LayoutParams startServerPrm=new FrameLayout.LayoutParams(0,0);
+		startServerPrm.width=-1;
+		startServerPrm.height=dpi2px(50);
+		startServerPrm.gravity=Gravity.CENTER_HORIZONTAL|Gravity.TOP;
+		startServerPrm.leftMargin=dpi2px(10);
+		startServerPrm.rightMargin=dpi2px(10);
+		startServerPrm.topMargin=dpi2px(8);
+	    startServer=new Button(this);
+		startServer.setText("Start Server");
+		frm.addView(startServer,startServerPrm);
+		
+		
+		ScrollView hScroll=new ScrollView(this);
+		FrameLayout.LayoutParams scrollParam=new FrameLayout.LayoutParams(-1,-1);
+		scrollParam.topMargin=dpi2px(64.761f);
+		scrollParam.bottomMargin=dpi2px(60);
+		scrollParam.leftMargin=dpi2px(2);
+		scrollParam.rightMargin=dpi2px(2);
+		hScroll.setBackgroundColor(Color.TRANSPARENT);
+		container=new LinearLayout(this);
+		container.setOrientation(LinearLayout.VERTICAL);
+		hScroll.addView(container);
+	
+		hScroll.setVerticalScrollBarEnabled(true);
+		frm.addView(hScroll,scrollParam);
+		
+		Button clean=new Button(this);
+		FrameLayout.LayoutParams cleanPrm=new FrameLayout.LayoutParams(0,0);
+		cleanPrm.width=-1;
+		cleanPrm.height=dpi2px(50);
+		cleanPrm.gravity=Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM;
+		cleanPrm.leftMargin=dpi2px(10);
+		cleanPrm.rightMargin=dpi2px(10);
+		cleanPrm.bottomMargin=dpi2px(2);
+		clean.setText("Clear Messages");
+		frm.addView(clean,cleanPrm);
+		setContentView(frm);
+		
+		startServer.setOnClickListener(new OnClickListener()
+		{
+            @Override
+			public void onClick(View v)
+			{
+				if(dgram==null)
+					  start();
+				else
+					  close();
+			}
+		});
+		clean.setOnClickListener(new OnClickListener()
+		{
+             @Override
+			 public void onClick(View v) {
+					container.removeAllViews();
+				}
+		});
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		close();
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause()
+	{
+		close();
+		super.onPause();
+	}
+
+	
+	
+}
